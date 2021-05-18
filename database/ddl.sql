f01ff86b252a37261978b2a21c904ebc608607c6
SamuelGoulart <62961331+SamuelGoulart@users.noreply.github.com>
2021-05-18 07:46:51 -0300

    Parte 3, categorias

---
+++ database/ddl.sql
@@ -0,0 +1,37 @@
+create database icatalogo;
+
+use icatalogo;
+
+create table tbl_produto(
+    id int primary key auto_increment,
+    descricao varchar(255) not null,
+    peso decimal not null,
+    quantidade int not null,
+    cor varchar(100) not null,
+    tamanho varchar(100),
+    valor decimal not null,
+    desconto int,
+    imagem varchar(500)
+);
+
+
+create table tbl_administrador(
+   id int primary key auto_increment,
+   nome varchar(100) not null,
+   usuario varchar(255) not null,
+   senha varchar (255) not null
+);
+
+insert into tbl_administrador (nome, usuario, senha) values ("Fulano de T
+al","fulano","123456"); 
+
+insert into tbl_administrador (nome, usuario, senha) values ("Ciclano da
+Silva","ciclano","654321"); 
+
+
+create table tbl_categoria (
+    id int primary key auto_increment,
+    descricao varchar(255) not null
+);
+
+insert into tbl_categoria(descricao) value ('Acessórios');
